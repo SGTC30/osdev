2a37a7e58a22acd0e7531cc9540d798937dd48cc
Sagacity Tech LLC <81399842+SGTC30@users.noreply.github.com>
2024-03-11 19:51:10 -0700

    Update boot.asm
    Add Hello

--- boot.asm
+++ boot.asm
@@ -1,9 +1,21 @@
 ;
 ; A simple boot sector.
 ;
-loop: ;define loop.
+mov ah, 0x0e ;define loop.
 
-jmp loop ;jump to loop address.
+mov al, 'H'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'O'
+int 0x10
+
+
+jmp $ ;jump to loop address.
 
 times 510-($-$$) db 0 ;padding to make 512 bytes.
 
